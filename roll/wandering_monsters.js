3680c2554f683840c4bd97926ba9f0b188dde731
Allen Vailliencourt <chumpmonkey@gmail.com>
2020-04-06 16:03:58 -0400

    Create wandering_monsters.js
    A simple macro I've created that allows you to auto-generate a wandering monster based on a d20 roll between 17-20.

---
+++ roll/wandering_monsters.js
@@ -0,0 +1,23 @@
+// setting variables
+let msgContent = 'Wandering Monster roll was: ';
+let result = '';
+
+// roll to check for wandering monster
+result = new Roll(`1d20`).roll().total;
+
+// create the message
+if(result !== '') {
+  let chatData = {
+    content: msgContent + result,
+    whisper: game.users.entities.filter(u => u.isGM).map(u => u._id)
+  };
+  ChatMessage.create(chatData, {});
+}
+
+// This requires you to have an existing Table built in Foundry VTT. Replace the table name below with your table name.
+// In this example, a roll between 17-20 will generate a roll from the Table. Tweak as needed!
+if (result >= 17) {
+  const table = game.tables.entities.find(t => t.name === 
+  "Wandering Monsters");
+  table.draw();
+}
