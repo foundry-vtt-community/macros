92b93e19f4dc1204fe86baa7034b4f170f9cfdf0
Shawn Dibble <shawndibble@gmail.com>
2020-05-05 14:45:12 -0500

    Stealth check against passive perception
    Rolls a stealth check again the selected token (or if player, their owned token).
    Then compares that stealth against the passive perception of all tokens on the map.
    Finally lists tokens who have a higher passive perception along with that value.

---
+++ roll/stealth_check.js
@@ -0,0 +1,54 @@
+// getting all actors of selected tokens
+let actors = canvas.tokens.controlledTokens.map(({ actor }) => actor);
+
+// if there are no selected tokens, roll for the player's character.
+if (actors.length < 1) {
+  actors = game.users.entities.map(entity => {
+    if (entity.active && entity.character !== null) {
+      return entity.character;
+    }
+  });
+}
+const validActors = actors.filter(actor => actor != null);
+
+let messageContent = 'pp = passive perception<br>';
+
+// roll for every actor
+for (const selectedActor of validActors) {
+  const stealthMod = selectedActor.data.data.skills.ste.mod; 
+  const stealth = new Roll(`1d20+${stealthMod}`).roll().total; 
+  messageContent += `<hr><h3>${selectedActor.name} stealth roll was a <b>${stealth}</b>.</h3>`;
+
+  // grab a list of unique tokens then check their passive perception against the rolled stealth.
+  const uniqueActor = {};
+  const caughtBy = canvas.tokens.placeables
+    .filter((token) => { // filter out duplicate token names. ie: we assume all goblins have the same passive perception
+      if (uniqueActor[token.actor.name]) {
+        return false;
+      }
+      uniqueActor[token.actor.name] = true;
+      return true;
+    })
+    .filter(token => {
+      return selectedActor.id !== token.actor.id; // Don't check to see if the token sees himself.
+    })
+    .filter(({ actor }) => actor.data.data.skills.prc.passive >= stealth); // check map tokens passives with roller stealth
+
+  if (!caughtBy.length) {
+    messageContent += 'Stealth successful!<br>';
+  } else {
+    messageContent += 'Stealth questionable:<br>';
+    caughtBy.map(({ actor }) => {
+      messageContent += `<b>${actor.name}</b> pp(${actor.data.data.skills.prc.passive}).<br>`;
+    });
+  }
+}
+
+// create the message
+const chatData = {
+  user: game.user._id,
+  speaker: game.user,
+  content: messageContent,
+  whisper: game.users.entities.filter((u) => u.isGM).map((u) => u._id),
+};
+ChatMessage.create(chatData, {});
