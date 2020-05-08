669bc8330ae919adf6210504e2818ef1b524beea
Shawn Dibble <shawndibble@gmail.com>
2020-05-08 16:12:37 -0500

    Create combat_tracker_ac_hp.js

---
+++ misc/combat_tracker_ac_hp.js
@@ -0,0 +1,10 @@
+// Adds the actor's AC to the combat tracker. Then toggles between HP and AC
+const a = "attributes.ac.value";
+const b = "attributes.hp.value";
+
+if (game.combat.settings.resource == a) {
+  game.settings.set('core', 'combatTrackerConfig', {resource: b, skipDefeated: true});
+} else {
+  game.settings.set('core', 'combatTrackerConfig', {resource: a, skipDefeated: true});
+}
+ui.combat.updateTrackedResources();
