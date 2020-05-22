a0a5abd6906963eaffd05eef6634e8c2f76f7fd6
Anthony Vadala <git@anthonyvadala.me>
2020-05-21 20:13:44 -0400

    Update actor/monk_ki.js
    Co-authored-by: Shawn Dibble <shawndibble@gmail.com>

--- actor/monk_ki.js
+++ actor/monk_ki.js
@@ -121,7 +121,7 @@
             "You can spend 1 ki point to take the <b>Disengage</b> or <b>Dash</b> action as a bonus action on your turn, and your jump distance is doubled for the turn.",
             2),
         new KiFeature("Deflect Missiles",
-            `Starting at 3rd level, you can use your reaction to deflect or catch the missile when you are hit by a ranged weapon attack. When you do so, the damage you take from the attack is reduced by 1d10 + your Dexterity modifier + your monk level.
+            `Starting at 3rd level, you can use your reaction to deflect or catch the missile when you are hit by a ranged weapon attack. When you do so, the damage you take from the attack is reduced by 1d10 + your Dexterity modifier + your monk level. <br />
 
         If you reduce the damage to 0, you can catch the missile if it is small enough for you to hold in one hand and you have at least one hand free. If you catch a missile in this way, you can spend 1 ki point to make a ranged attack with the weapon or piece of ammunition you just caught, as part of the same reaction. You make this attack with proficiency, regardless of your weapon proficiencies, and the missile counts as a monk weapon for the attack, which has a normal range of 20 feet and a long range of 60 feet.`,
             3,
