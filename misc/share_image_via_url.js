a8a67cb7343d2d744ee1ccaa29653f63e15827d0
Shawn Dibble <shawndibble@gmail.com>
2020-06-01 21:09:12 -0500

    Update share_image_via_url.js
    Add more options for sharing the image.

--- misc/share_image_via_url.js
+++ misc/share_image_via_url.js
@@ -1,16 +1,44 @@
 /**
- * Share an image to all players when you have an image URL
+ * Share an image to all when you have an image URL.
+ * GMs can share to a popup, chat or both.
+ * Players can only share to chat.
  * Author: @Krishmero#1792
  */
+
+let imagePopup = (imageUrl) => {
+  // Display the image popout and share it.
+  const ip = new ImagePopout(imageUrl);
+  ip.render(true);
+  ip.shareImage();
+};
+
+let chatDialog = (imageUrl) => {
+	ChatMessage.create({
+    user: game.user._id,
+    content: `<img src="${imageUrl}" />`,
+    type: CONST.CHAT_MESSAGE_TYPES.OOC
+  });
+};
+
+let selectOptions = game.user.isGM ? `
+<label for="output-options" style="margin-right: 10px">Output Options:</label>
+<select id="output-options" />
+  <option value="popup">Popup</option>
+  <option value="chat">Chat</option>
+  <option value="both">Both</option>
+</select>
+` : null
 
 new Dialog({
   title: `Share Image via URL`,
   content: `
     <form>
-      <div class="form-group">
-        <label for="image-url">Image URL:</label>
-        <input type="text" id="image-url" name="image-url" />
-      </div>
+      <div style="display: inline-block; width: 100%; margin-bottom: 10px">${selectOptions}</div>
+      <br />
+      <div style="display: flex; width: 100%; margin-bottom: 10px">
+      	<label for="image-url" style="white-space: nowrap; margin-right: 10px; padding-top:4px">Image URL:</label>
+      	<input type="text" id="image-url" name="image-url" />
+    	</div>
     </form>
   `,
   buttons: {
@@ -18,17 +46,17 @@ new Dialog({
       icon: "<i class='fas fa-check'></i>",
       label: `Share`,
       callback: (html) => {
-        let imageUrl = html.find('#image-url').val();
-        if (!imageUrl) {
-          return ui.notifications.info("You did not provide a valid image.");
+				let imageUrl = html.find('#image-url').val();
+				let permission = html.find('select#output-options')[0].value;
+			  if (!imageUrl) {
+			    return ui.notifications.info("You did not provide a valid image.");
+			  }
+        if (game.user.isGM && ['popup', 'both'].includes(permission)) {
+        	imagePopup(imageUrl);
         }
-        const ip = new ImagePopout(imageUrl);
-
-        // Display the image popout
-        ip.render(true);
-
-        // Share the image with other connected players
-        ip.shareImage();
+        if (!game.user.isGM || ['chat', 'both'].includes(permission)) {
+        	chatDialog(imageUrl);
+        }
       }
     },
     no: {
