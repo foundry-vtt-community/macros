92b6aa0856fbf5c433b0b7e52220329b99a8920f
Shawn Dibble <shawndibble@gmail.com>
2020-06-24 12:24:14 -0500

    Update import_from_compendium.js
    make more dynamic

--- misc/import_from_compendium.js
+++ misc/import_from_compendium.js
@@ -1,9 +1,10 @@
 /** 
- * Import all the actors from a compendium. Change game.actors to game.items for items or to game.journal for journal entires.
+ * Import all the actors from a compendium.
  * In order to find the packName, you can use the following in your console (F12): game.packs.map(p => p.collection);
  * Author: KrishMero#1702
  */
  
 let packName = 'dnd5e.heroes';
 let pack = game.packs.get(packName);
-pack.index.forEach(entry => game.actors.importFromCollection(packName, entry._id);
+let type = pack.metadata.entity.toLowerCase() + 's'; //grab the type and pluralize it.
+pack.index.forEach(entry => game[type].importFromCollection(packName, entry._id));
