850c10f459c31f1a9a8d288baf4f9e2643f49729
Zapgun <330793+Zapgun@users.noreply.github.com>
2020-07-02 12:48:03 +0100

    Equip shield macro upgrade
    Upgraded the equip shield script to be more robust - now includes error checking and chat feedback options.

--- item/equip_unequip_shield.js
+++ item/equip_unequip_shield.js
@@ -1,20 +1,41 @@
 /**
- * Equips or unequips an item named 'Shield'. Then updates the character's AC.
- * Author: ^ and stick#0520
+ * Equips/unequips an item. Make sure you change the variables at the top (as required).
  */
 
-let charData = game.user.character;
-let shield = charData.items.find(i => i.name == 'Shield');
+let itemName = 'Shield'; // <--- Change this to the *exact* item name (capitals count!)
+let sendToChat = true; // <--- Change to 'true' or 'false' to display a chat message about equipping
 
-if (shield != null)
-{
-   let item = game.user.character.getOwnedItem(shield._id);
-   let attr = "data.equipped";
-   item.update({[attr]: !getProperty(item.data, attr)});
+if (!actor) {
+    ui.notifications.warn('You need to select a token before using this macro!');
+} else {
 
-   if (getProperty(item.data, attr)) {
-      charData.update({"data.attributes.ac.value": charData.data.data.attributes.ac.value-2});
-   } else {
-      charData.update({"data.attributes.ac.value": charData.data.data.attributes.ac.value+2});
-   }
+	let myItem = actor.items.find(i => i.name == itemName);
+	if (myItem != null)
+	{
+		let item = actor.getOwnedItem(myItem._id);
+		let attr = "data.equipped";
+		let equipped = getProperty(item.data, attr);
+		if (sendToChat) {			
+			if (!equipped) {
+				chatMessage(actor.name + ' <b>equips</b> their <i>' + ' ' + itemName+ '</i>');
+			} else {
+				chatMessage(actor.name + ' <b>un-equips</b> their <i>' + ' ' + itemName + '</i>');			
+			}
+		}
+		item.update({[attr]: !getProperty(item.data, attr)});
+	} else {
+		ui.notifications.warn("No item named '" + itemName + "' found on character!");
+	}
+}
+
+function chatMessage(messageContent) {
+	// create the message
+	if (messageContent !== '') {
+		let chatData = {
+			user: game.user._id,
+			speaker: ChatMessage.getSpeaker(),
+			content: messageContent,
+		};
+		ChatMessage.create(chatData, {});
+	}
 }
