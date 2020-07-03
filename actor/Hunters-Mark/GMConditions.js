8e15cee1aaeaa58ae4743e6d3ecede4df0621066
Naokotani <thehughesdude@gmail.com>
2020-07-02 22:07:42 -0300

    Update GMConditions.js

--- actor/Hunters-Mark/GMConditions.js
+++ actor/Hunters-Mark/GMConditions.js
@@ -1,3 +1,9 @@
+//This macro must be called 'GMConditions' and be on the GM's
+//hot bar for the castMark.js macro to function correctly.
+//you must tick the option 'Excecute Macro As GM' to grant
+//players access to this macro.
+
+
 const action = args[0]
 const condition = args[1]
 const targetId = args[2]
