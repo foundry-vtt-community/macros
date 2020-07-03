51647cf8a2d345507e5d5f280536852ff8e2d2cf
Shawn Dibble <shawndibble@gmail.com>
2020-07-02 19:39:06 -0500

    Update to be more user friendly

--- misc/import_from_compendium.js
+++ misc/import_from_compendium.js
@@ -4,8 +4,54 @@
  * Author: KrishMero#1702
  */
  
-let packName = 'dnd5e.heroes';
-let pack = game.packs.get(packName);
-let type = pack.metadata.entity.toLowerCase();
-type = game[type] ? type : type + 's'; //check if the type exists, otherwise pluralize it.
-pack.index.forEach(entry => game[type].importFromCollection(packName, entry._id));
+let packOptions = game.packs.map(pack => `<option value="${pack.collection}">${pack.title}</option>`);
+const form = `
+  <div style="display: inline-block; width: 100px">Folder:</div>
+  <input type="string" id="folderName">
+  <br />
+  <div style="font-size: 80%">leave blank to put into root directory</div>
+  <br />
+
+  <div style="display: inline-block; width: 100px">Compendium:</div>
+  <select id="destinationPack" />
+    ${packOptions}
+  </select>
+  <br />
+
+  <label>
+    <input type="checkbox" id="delete"/>
+    Clear destination first
+  </label>
+`;
+
+const dialog = new Dialog({
+  title: "Import data from compendium",
+  content: form,
+  buttons: {
+    use: {
+      label: "Apply",
+      callback: importCompendium
+    }
+  }
+}).render(true);
+
+function importCompendium(html) {
+  const folderName = html.find(`input#folderName`)[0].value;
+  const packName = html.find(`select#destinationPack`)[0].value;
+  const remove = html.find(`input#delete`)[0].checked;
+
+  let pack = game.packs.get(packName);
+  let folder = game.folders.find(f => f.name === folderName && f.type === pack.entity)?.id;
+  let type = pack.metadata.entity.toLowerCase();
+  type = type === 'journalentry' ? 'journal' : type + 's';
+  let extra = folder ? { folder } : null
+
+  if (folderName && !folder) {
+    return ui.notifications.error(`Your world does not have any ${type} folders named '${folderName}'.`);
+  }
+
+  if (remove) {
+    game[type].filter(t => t.data.folder === folder)?.delete();
+  }
+  pack.getIndex().then(index => index.forEach(entry => game[type].importFromCollection(packName, entry._id, extra)));
+}
