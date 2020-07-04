b8f20f0129b4d45b24343bbc661fb48db6581389
Drunemeton <63975886+Drunemeton@users.noreply.github.com>
2020-07-04 10:33:30 -0700

    Update all_tokens_passive_perception.js
    I've revamped the entire macro to segregate the data into PC and NPC message parts that are then sorted by name, and then passive perception value.

--- token/all_tokens_passive_perception.js
+++ token/all_tokens_passive_perception.js
@@ -1,34 +1,73 @@
 // Pull the passive perception of each token in the current scene and whisper the results to the GM.
-// This has only been tested within the D&D 5e system.
+// Only tested with the 5e System in Foundry.
 // Author: @Drunemeton#7955. Based on the original macro by author @Erogroth#7134.
 
-// Collect tokens in the scene.
+// Initalize variables.
+let pcArray = [];
+let npcArray = [];
+let messageContentPC = "";
+let messageContentNPC = "";
+let messageHeaderPC = "<b>PC Passive Perception</b><br>";
+let messageHeaderNPC = "<b>NPC Passive Perception</b><br>";
+
+// Gather tokens in the current scene into an array.
 let tokens = canvas.tokens.placeables.filter((token) => token.data);
 
-// Initialize message building blocks.
-let messageContent = "";
-let messageHeader = "<b>Passive Perception</b><br>";
-
-// Loop through the token array.
+// From the tokens array sort into PC and NPC arrays.
 for (let count of tokens) {
-  // Extract the passive perception value.
-  let pp = count.actor.data.data.skills.prc.passive;
-  if (typeof pp !== "number") {
-    // Create the output string.
-    messageContent += `${count.name} <b>${pp}</b><br>`;
-  } else {
-    messageContent += `${count.name} <b>Unknown</b><br>`;
+  let tokenType = count.actor.data.type;
+  let tokenName = count.data.name;
+  let tokenPassive = count.actor.data.data.skills.prc.passive;
+  
+  if(tokenType === "character") {
+    pcArray.push({ name: tokenName, passive: tokenPassive });
+  } 
+  if(tokenType === "npc") {
+    npcArray.push({ name: tokenName, passive: tokenPassive });
   }
 }
 
-// Create the message.
-if (messageContent !== "") {
-  let chatData = {
-    user: game.user._id,
-    speaker: ChatMessage.getSpeaker(),
-    content: messageHeader + messageContent,
-    whisper: game.users.entities.filter((u) => u.isGM).map((u) => u._id),
-  };
-  // Display message in chat.
-  ChatMessage.create(chatData, {});
+// Sort each array.
+sortArray(pcArray);
+sortArray(npcArray);
+
+// Build chat message, with PCs first, then NPCs.
+for (let numPC of pcArray) {
+  messageContentPC += `${numPC.name}: <b>${numPC.passive}</b><br>`;
 }
+for (let numNPC of npcArray) {
+  messageContentNPC += `${numNPC.name}: <b>${numNPC.passive}</b><br>`;
+}
+
+let chatMessage = (messageHeaderPC + messageContentPC + `<br>` + messageHeaderNPC + messageContentNPC);
+
+let chatData = {
+  user: game.user._id,
+  speaker: ChatMessage.getSpeaker(),
+  content: chatMessage,
+  whisper: game.users.entities.filter((u) => u.isGM).map((u) => u._id),
+};
+
+// Display chat message.
+ChatMessage.create(chatData, {});
+
+// Sort each array by Name.
+  function sortArray(checkArray) {
+    checkArray.sort(function (a, b) {
+      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      // names must be equal
+      return 0;
+    });
+
+    // Sort array by Passive Perception.
+    checkArray.sort(function (a, b) {
+      return b.passive - a.passive;
+    });
+  }
