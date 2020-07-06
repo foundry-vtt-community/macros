031c2e51752cd26a3ab18ab63d0cc54166d4dddb
Shawn Dibble <shawndibble@gmail.com>
2020-07-06 10:30:51 -0500

    Update delete_all_templates.js

--- misc/delete_all_templates.js
+++ misc/delete_all_templates.js
@@ -2,4 +2,8 @@
  * Deletes all templates on the current scene
  */
  
- canvas.templates.deleteMany(canvas.templates.placeables.map(o =>o.id),{});
+// no dialog. Just delete all templates.
+canvas.templates.deleteMany(canvas.templates.placeables.map(o =>o.id),{});
+
+// Get a dialog confirmation before deleting all templates on the scene:
+// canvas.templates.deleteAll()
