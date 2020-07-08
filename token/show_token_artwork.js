4f304882e83a1e5946435b4136072798da561fd0
Shawn Dibble <shawndibble@gmail.com>
2020-07-08 17:32:16 -0500

    Update show_token_artwork.js

--- token/show_token_artwork.js
+++ token/show_token_artwork.js
@@ -10,9 +10,10 @@ if (token === undefined) {
   ui.notifications.warn("Please select token first.");
 } else {
   let tActor = token.actor;
-  new ImagePopout(tActor.data.img, {
+  let ip = new ImagePopout(tActor.data.img, {
     title: tActor.name,
     shareable: true,
     uuid: tActor.uuid
   }).render(true);
+  ip.shareImage();
 }
