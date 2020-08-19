61237bc70caea9ffd8e531555cd8b1692163b05d
Vance Cole <coletain@gmail.com>
2020-08-19 14:44:02 -0700

    Update tile_toggle_hidden_status.js
    Removes furnace requirement, simplifies macro

--- misc/tile_toggle_hidden_status.js
+++ misc/tile_toggle_hidden_status.js
@@ -1,29 +1,14 @@
-//Simple macro to loop through ALL SELECTED TILES and toggle whether or not they are hidden.
-//In other words:
-    //If an individual tile is visible, this macro will hide it.
-    //If an individual tile is hidden, this macro will make it visible.
-//Bonus function to hide or unhide tiles instead also included.
+// Simple macro to loop through ALL SELECTED TILES and toggle whether or not they are hidden.
+// Uncomment line 8 or 9 to change behavior to hide / show all tiles instead of toggle
 
-async function toggleTileVisibility(tile) {
-    console.log(tile.data.hidden);
-    console.log(!tile.data.hidden);
-    await tile.update({hidden: !tile.data.hidden});
-    }
-
-async function setTileHidden(tile, trueOrFalse) {
-    await tile.update({hidden: trueOrFalse});
-}
-
-//check to make sure at least one tile is selected
 if (canvas.tiles.controlled[0]) {
-    //loop through all selected tiles
-    for (let t of canvas.tiles.controlled) {
-        await toggleTileVisibility(t);
-        //hide all selected tiles instead
-        //await setTileHidden(t,true);
-        //show all selected tiles instead
-        //await setTileHidden(t,false);
-    }
+  canvas.tiles.controlled.forEach(tile => {
+    let v;
+    v = !tile.data.hidden; // Toggle visibility for each tile
+    // v = false;          // Hide all selected tiles
+    // v = true;           // Show all selected tiles
+    tile.update({ hidden: v });
+  });
 } else {
-    ui.notifications.notify("Please select at least one tile.");
+  ui.notifications.notify("Please select at least one tile.");
 }
