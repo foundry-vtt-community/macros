aff8b9cbd401eaccfe29dd6a32629b3299218eac
Mattias Lepp <lepp.mattias@gmail.com>
2020-11-13 22:36:13 +0100

    Evolution of light_picker.js with added color option.
    Also some restructuring to what I feel is a more extendable structure.

---
+++ token/light_picker_color.js
@@ -0,0 +1,76 @@
+function tokenUpdate(data) {
+    canvas.tokens.controlled.map(token => token.update(data));
+}
+  
+
+let color = "#ffffff";
+let tokens = canvas.tokens.controlled;
+if (tokens.length === 1) {
+    color = tokens[0].data.lightColor;
+}
+
+const torchAnimation = {type: "torch", speed: 1, intensity: 1};
+const energyShield = {type: "energy", speed: 1, intensity: 1};
+const lights = {
+    none: {
+        label: "None",
+        data: {dimLight: null, brightLight: null, lightAngle: 360}
+    },
+    torch: {
+        label: "Torch",
+        data: {dimLight: 40, brightLight: 20, lightAngle: 360, lightAnimation: torchAnimation}
+    },
+    light: {
+        label: "Light cantrip",
+        data: {dimLight: 40, brightLight: 20, lightAngle: 360, lightAnimation: {type: "none"}}
+    },
+    lamp: {
+        label: "Lamp",
+        data: {dimLight: 45, brightLight: 15, lightAngle: 360, lightAnimation: torchAnimation}
+    },
+    shield: {
+        label: "Shield",
+        data: {dimLight: 0.5, brightLight: 0, lightAngle: 360, lightAnimation: energyShield}
+    },
+    bullseye: {
+        label: "Bullseye Lantern",
+        data: {dimLight: 120, brightLight: 60, lightAngle: 45, lightAnimation: torchAnimation}
+    }
+};
+
+function getLights() {
+    let lightButtons = {};
+    Object.entries(lights).forEach(([key, light]) => {
+        lightButtons[key] = {
+            label: light.label,
+            callback: (html) => {
+                const newColor = html.find("#color").val();
+                var data = light.data;
+                tokenUpdate(Object.assign(data, {lightColor: newColor}));
+            }
+        }
+    });
+
+    lightButtons = Object.assign(lightButtons, {
+        close: {
+            icon: "<i class='fas fa-tick'></i>",
+            label: `Close`
+        }
+    });
+
+    return lightButtons;
+}
+
+new Dialog({
+    title: `Token Light Picker`,
+    content: `
+        <form>
+        <div style="display: flex; align-content: center;">
+        <label for="color" style="line-height: 25px;">Color:</label>
+        <input type="color" value="${color}" id="color" style="margin-left: 10px;">
+        </div>
+        </form>`,
+    buttons: getLights(),
+    default: "close",
+    close: () => {}
+}).render(true);
