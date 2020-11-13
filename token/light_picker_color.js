bfa2b8582a1659e8a6152468fe873f46e3be69f7
Mattias Lepp <lepp.mattias@gmail.com>
2020-11-13 22:59:36 +0100

    Added slider for color intensity. GM ONLY

--- token/light_picker_color.js
+++ token/light_picker_color.js
@@ -1,12 +1,15 @@
 function tokenUpdate(data) {
     canvas.tokens.controlled.map(token => token.update(data));
 }
-  
+
+const isGM = game.user.isGM;
 
 let color = "#ffffff";
+let alpha = 1.0;
 let tokens = canvas.tokens.controlled;
 if (tokens.length === 1) {
     color = tokens[0].data.lightColor;
+    alpha = tokens[0].data.lightAlpha;
 }
 
 const torchAnimation = {type: "torch", speed: 1, intensity: 1};
@@ -45,8 +48,9 @@ function getLights() {
             label: light.label,
             callback: (html) => {
                 const newColor = html.find("#color").val();
+                const newAlpha = html.find("#alpha").val();
                 var data = light.data;
-                tokenUpdate(Object.assign(data, {lightColor: newColor}));
+                tokenUpdate(Object.assign(data, {lightColor: newColor, lightAlpha: newAlpha}));
             }
         }
     });
@@ -65,10 +69,12 @@ new Dialog({
     title: `Token Light Picker`,
     content: `
         <form>
-        <div style="display: flex; align-content: center;">
-        <label for="color" style="line-height: 25px;">Color:</label>
-        <input type="color" value="${color}" id="color" style="margin-left: 10px;">
-        </div>
+            <div style="display: flex; align-content: center;">
+            <label for="color" style="line-height: 25px;">Color:</label>
+            <input type="color" value="${color}" id="color" style="margin-left: 10px;">
+            ${isGM ? '<label for="alpha" style="line-height: 25px;">Intensity:</label>' : ''}
+            <input type="${isGM ? 'range' : 'hidden'}" value="${alpha}" id="alpha" style="margin-left: 10px;" min="0.0" max="1.0" step="0.05">
+            </div>
         </form>`,
     buttons: getLights(),
     default: "close",
