e82f5bad8f9dc0fa3d8a9deb4e91f107d6ae9f9f
Lyle Reed <lyleareed@gmail.com>
2020-11-17 13:14:48 -0600

    Use new CUB syntax in Hunter Mark Macro
    Combat Utility Belt no longer uses the `game.cub.applyCondition` function.
    
    Updated with the new `game.cub.addCondition` call.

--- module-specific/hunters_mark-gm_conditions.js
+++ module-specific/hunters_mark-gm_conditions.js
@@ -9,7 +9,7 @@ const condition = args[1]
 const targetId = args[2]
 const target = canvas.tokens.get(targetId);
 if (action == "apply"){
-    game.cub.applyCondition(condition, target, {warn: true});
+    game.cub.addCondition(condition, target, {replaceExisting: true})
 }
 else if (action == "remove"){
     game.cub.removeCondition(condition, target, {warn: true});
