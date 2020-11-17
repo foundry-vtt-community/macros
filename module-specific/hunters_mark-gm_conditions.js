44705ebe84f703f0d0a975f03d68e33e3c06184e
Lyle Reed <lyleareed@gmail.com>
2020-11-17 13:15:38 -0600

    Adjust Hunter Mark GM macro instructions
    Add text about how to create a GM macro in your game.

--- module-specific/hunters_mark-gm_conditions.js
+++ module-specific/hunters_mark-gm_conditions.js
@@ -2,6 +2,9 @@
 //hot bar for the castMark.js macro to function correctly.
 //you must tick the option 'Excecute Macro As GM' to grant
 //players access to this macro.
+//To do this you must create a new macro in your game and use
+//the code below, dragging the macro from the compendium
+//will not work.
 
 
 const action = args[0]
