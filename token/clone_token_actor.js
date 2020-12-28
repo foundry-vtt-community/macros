40a8f45aa1e80bb0cc659b3cfe24cdb30f8a7f57
herrtoke <76668646+herrtoke@users.noreply.github.com>
2020-12-27 19:13:59 -0700

    Create clone_token_actor.js
    Clones actor details from the selected token(s) into a new Actor in the item list. Useful if you made changes to the actor associated with the token, but want to save that updated Actor for later use or into a Compendium.

---
+++ token/clone_token_actor.js
@@ -0,0 +1,10 @@
+// Clones actor details from the selected token(s) into a new Actor in the item list.
+// Useful if you made changes to the actor associated with the token, but want to save that
+//  updated Actor for later use or into a Compendium.
+// Created Actor will default to the name of the token with the actorNameSuffix (default: '_cloned')
+
+const actorNameSuffix = "_cloned";
+
+canvas.tokens.controlled.forEach(o => {
+  o.actor.clone({ name: o.name + actorNameSuffix });
+});
