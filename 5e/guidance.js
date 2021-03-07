85e63a479966882387e4d08cc55d2495bcef301f
Penguin-0949 <71253027+Penguin-0949@users.noreply.github.com>
2021-03-07 18:20:51 +0000

    Update guidance.js
    updated to use Effects

--- 5e/guidance.js
+++ 5e/guidance.js
@@ -1,60 +1,51 @@
 // new build for guidance macro by Mr.White and Penguin#0949 and with no help all of Kotetsushin#7680 trust me 
-// Edited by DrWiFi to roll guidance on skill checks as well, removed references to bless spell
-// version beta 3.1.1 for workgroups .2
+// version beta 4.2.0
 
 // user notes
-// this macro is intended for use by the recipient of the guidance spell in D&D 5e on Forge VTT
-// N.B. every recipient will need to use this macro independently on their own Actor/token.
+// this macro is inteded for use by the recipient of the bless spell in D&D 5e on Forge VTT
+// N.B. every recipient will need to use this macro independantly on their own Actor/token.
 
 //user modifiable declarations CHANGE AT YOUR OWN RISK
-const guidanceIconPath = 'icons/svg/windmill.svg';
-let guidanceMsg = ' is guided!';
-let endguidanceMsg = ' is no longer guided.';
+const GuidIconPath = 'icons/svg/windmill.svg';
+let GuideMsg = ' is guided!';
+let endGuideMsg = ' is no longer guided.';
 
 //fixed declarations DO NOT MODIFY
-let Guidd4 = '+1d4';
-let guided = '';
 let chatMsg = '';
-let macroActor = actor;
-let macroToken = token;
-
-//identify token
-if (macroToken === undefined || macroToken === null) {
-  ui.notifications.warn("Please select a token first.");
-} else {
-// grab curent global states
-	let abilities = JSON.parse(JSON.stringify(macroActor.data.data.bonuses.abilities.check));
-	if(abilities.includes(Guidd4)){
-		guided = true;
-	}
-// If not already guided	
-	if (guided == false || guided === null || guided === undefined || guided == "") {	
-// toggle guidance icon
-		macroToken.toggleEffect(guidanceIconPath);  
-// anounce to chat
-		chatMsg = `${macroActor.name} ${guidanceMsg}`;
-// add guidance bonus
-		console.log('adding guidance modifiers to global bonuses');
-		let obj = {};
-		obj['data.bonuses.abilities.check'] = abilities + Guidd4;
-		macroActor.update(obj);
-// if already guided	
-	}	else if (guided == true) {
-// toggle guidance icon
-		token.toggleEffect(guidanceIconPath);  		
-// anounce to chat
-		chatMsg = `${macroActor.name} ${endguidanceMsg}`;
-// remove guidance bonus
-		console.log('resetting global bonuses for guidance');
-		let obj = {};
-		var tmp = JSON.parse(JSON.stringify(macroActor.data.data.bonuses.abilities.check));
-		var tmpLength = tmp.indexOf(Guidd4);
-        tmp = tmp.substring(0, tmpLength) + tmp.substring(tmpLength+4, tmp.length);
-		obj['data.bonuses.abilities.check'] = tmp;
-		macroActor.update(obj);
-	}
+let macroActor = token.actor;
+let Guided = macroActor.effects.find(i => i.data.label === "Guided")
+let Guide = {
+    changes: [
+        {
+            key: "data.bonuses.abilities.check",
+            mode: 2,
+            priority: 20,
+            value: "+1d4",
+        },
+    ],
+    duration: {
+        "seconds": 6,
+    },
+    icon: GuidIconPath,
+    label: "Guided"
 }
-  	
+//identify token
+if (macroActor === undefined || macroActor === null) {
+  ui.notifications.warn("Please select a token first.");
+}
+else {
+// If  already guided	
+if (Guided) {
+    macroActor.deleteEmbeddedEntity("ActiveEffect", Guided.id)
+// anounce to chat
+	chatMsg = `${macroActor.name} ${endGuideMsg}`;
+}
+// if not already guided	
+else {
+    macroActor.createEmbeddedEntity("ActiveEffect", Guide)	
+// anounce to chat
+		chatMsg = `${macroActor.name} ${GuideMsg}`;
+}
 // write to chat if needed:
 if (chatMsg !== '') {
 	let chatData = {
@@ -63,4 +54,5 @@ if (chatMsg !== '') {
 		content: chatMsg
 	};
 	ChatMessage.create(chatData, {});
+}
 }
