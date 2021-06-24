dae877d8c5f2b0c653d37bb9470f63d2934c515a
Anthony Vadala <git@anthonyvadala.me>
2021-06-24 12:37:57 -0400

    Added change from issue #222

--- misc/create_chat_message.js
+++ misc/create_chat_message.js
@@ -1,5 +1,6 @@
 // Courtesy of @errational
 // Creates a chat message.
+let controlledToken = canvas.tokens.controlled[0];
 const content = `<p>Monster attacks ${controlledToken.name}</p>`;
 
 ChatMessage.create({
