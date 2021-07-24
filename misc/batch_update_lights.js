d9693a6709e7c0811dc50c5df0384fb9b9f804d2
Stephen Cooper <scooper4711@users.noreply.github.com>
2021-07-24 11:55:23 -0600

    A macro for normalizing all lights after import

---
+++ misc/batch_update_lights.js
@@ -0,0 +1,28 @@
+// Based on find_lights_by_color.js
+// Courtesy of @FloRad
+// This macro is intended to perform a batch update
+// of all lights in the current scene e.g. after
+// importing from Unversal Battle Map importer,
+// allowing you to set e.g. all wall torches identically.
+(async () => {
+        let foundLights = [];
+        let markingColor = "#eccd8b"
+        let newColor = "#fec80a"
+        let scene = game.scenes.active;
+    
+        canvas.lighting.placeables.forEach(l => { if (l.data.tintColor === markingColor && l.scene === scene) foundLights.push(l.id) })
+    
+        const updates = []
+        foundLights.forEach(id => {
+            updates.push({ _id: id, 
+                            tintColor: newColor, 
+                            darkness: {min:0.2, max:1.0}, 
+                            dim: 10, 
+                            bright: 5, 
+                            lightAnimation: {speed: 1, intensity: 3, type: "torch"} });
+        })
+    
+        await scene.updateEmbeddedEntity("AmbientLight", updates);
+    
+        console.log(foundLights)
+    })()
