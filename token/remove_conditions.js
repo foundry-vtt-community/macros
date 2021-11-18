396a703a09d8f3ef5c8979b2af73cf1ef1f55753
Cody <cody@zornes.dev>
2021-11-18 13:42:41 -0800

    Update to work with ActiveEffects
    This macro didn't seem to work with newer versions of foundry since the addition of ActiveEffects into the core app. This should properly delete the effect from the actor sheets for each selected token, which will update the token correspondingly.

--- token/remove_conditions.js
+++ token/remove_conditions.js
@@ -1,5 +1,6 @@
 (async () => {
-  for ( let token of canvas.tokens.controlled ){
-    await token.update({"effects": []});
-  }
+  for ( let token of canvas.tokens.controlled ) {
+    let effectsToDelete = token.actor.effects.filter(e => e.sourceName === "None")
+      .map(e => { return e.id }); // documents api expects array of ids
+    await token.actor.deleteEmbeddedDocuments("ActiveEffect", effectsToDelete);
 })();
