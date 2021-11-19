87831da0434d807438dabce47bc0b5360bda1a98
Anthony Vadala <git@anthonyvadala.me>
2021-11-19 10:19:18 -0500

    Fixed syntax error

--- token/remove_conditions.js
+++ token/remove_conditions.js
@@ -3,4 +3,4 @@
     let effectsToDelete = token.actor.effects.filter(e => e.sourceName === "None")
       .map(e => { return e.id }); // documents api expects array of ids
     await token.actor.deleteEmbeddedDocuments("ActiveEffect", effectsToDelete);
-})();
+}})();
