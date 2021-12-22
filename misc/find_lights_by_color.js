2b9d910e438d3d230694508e55cf8384f6bedcd6
Freeze021 <84507769+Freeze021@users.noreply.github.com>
2021-12-22 20:28:03 +0100

    Update find_lights_by_color.js

--- misc/find_lights_by_color.js
+++ misc/find_lights_by_color.js
@@ -1,27 +1,25 @@
-// Courtesy of @FloRad, Updated by scooper4711 
+// Courtesy of @FloRad, Updated by scooper4711, updated for v9 by Freeze#2689
 // This macro is intended to perform a batch update
 // of all lights in the current scene e.g. after
 // importing from Unversal Battle Map importer,
 // allowing you to set e.g. all wall torches identically.
-(async () => {
-        let foundLights = [];
-        let markingColor = "#eccd8b"
-        let newColor = "#fec80a"
-        let scene = game.scenes.active;
-    
-        canvas.lighting.placeables.forEach(l => { if (l.data.tintColor === markingColor && l.scene === scene) foundLights.push(l.id) })
-    
-        const updates = []
-        foundLights.forEach(id => {
-            updates.push({ _id: id, 
-                            tintColor: newColor, 
-                            darkness: {min:0.2, max:1.0}, 
-                            dim: 10, 
-                            bright: 5, 
-                            lightAnimation: {speed: 1, intensity: 3, type: "torch"} });
-        })
-    
-        await scene.updateEmbeddedEntity("AmbientLight", updates);
-    
-        console.log(foundLights)
-    })()
+let markingColor = "#ff0000"//"#eccd8b"
+let newColor = "#fec80a"
+let scene = game.scenes.active;
+
+const foundLights = canvas.lighting.placeables.reduce((acc, l) => { 
+    if (l.data.config.color === markingColor && l.scene.id === scene.id) acc.push(l) 
+    return acc
+}, []);
+const updates = foundLights.map(light => ({ 
+    _id: light.id, 
+    config: {
+        color: newColor, 
+        darkness: {min:0.2, max:1.0}, 
+        dim: 10, 
+        bright: 5, 
+        animation: {speed: 1, intensity: 3, type: "torch"} 
+    }
+}));
+
+await scene.updateEmbeddedDocuments("AmbientLight", updates);
