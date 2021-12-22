63bbdcb8051188b5d5c269fa505d1a91722d8e1b
Freeze021 <84507769+Freeze021@users.noreply.github.com>
2021-12-22 20:33:53 +0100

    Update folder_to_rollable_table.js

--- misc/folder_to_rollable_table.js
+++ misc/folder_to_rollable_table.js
@@ -1,26 +1,32 @@
-// Take the entries of a folder and turn it into a rollable table.
-// NOTE: you must click the "Normalize Result Weights" button (scale icon) after import.
-// Author: @Atropos#3814
+// Take the entries of a folder and turn it into a rollable table. 
+// If the table exists it appends the results, if no table exists the table is created. Change the description to fit your needs.
+// Author: @Atropos#3814 edited for v9 by Freeze#2689
 
-// name of folder whos entities you wish to push into a rollable table.
-const folder = game.folders.getName("Herbalism & Alchemy");
-// name of table you will be overwriting
-const table = game.tables.getName("Common Ingredients");
-// change this to match the entity type you are importing.
-// Actor, Item, Scene, JournalEntry, Macro, RollTable, Playlist
-const entityType = "Item"
+const tableName = "some table name"; // name of table you will be appending, or creating.
+const folderName = "some folder"; // name of folder whos Documents you wish to push into a rollable table.
 
-const items = folder.entities;
-const results = folder.entities.map(i => {
-  return {
-    text: i.data.name,
-    type: 1,
-    collection: "Item",
-    resultId: i.data._id,
-    img: i.data.img,
-    weight: 1,
-    range: [1, 1],
-    drawn: false
-  }
+const folder = game.folders.getName(folderName);
+const table = game.tables.getName(tableName);
+const results = folder.contents.map((i, count) => {
+    return {
+        text: i.name,
+        type: CONST.TABLE_RESULT_TYPES.DOCUMENT,
+        collection: folder.type,
+        resultId: i.id,
+        img: i.img,
+        weight: 1,
+        range: [count + 1, count + 1],
+        drawn: false
+    }
 });
-await table.createEmbeddedEntity("TableResult", results);
+if(table){
+    await table.createEmbeddedDocuments("TableResult", results);
+}
+else {
+    table = await RollTable.createDocuments([{
+        name: tableName,
+        description: "table description",
+        results: results,
+        formula: `1d${results.length}`
+    }]);
+}
