e513bb105688819ed9741c340fafb557b0787e91
Chris Seieroe <kaelad02@users.noreply.github.com>
2022-02-21 16:03:22 -0800

    await roll in fighter_second_wind

--- 5e/fighter_second_wind.js
+++ 5e/fighter_second_wind.js
@@ -41,7 +41,7 @@ if (macroActor !== undefined && macroActor !== null) {
     if (checkResource(macroActor)) {
       // Calculate string to roll based on fighter level
       let fighterLvl = fighter.data.data.levels;
-      let healRoll = new Roll(`1d10 + ${fighterLvl}`).roll();
+      let healRoll = await new Roll(`1d10 + ${fighterLvl}`).roll();
 
       ChatMessage.create({
         user: game.user._id,
