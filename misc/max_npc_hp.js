ea5015d64c827abac66ef01c30a4a2f39282bba4
Chris Seieroe <kaelad02@users.noreply.github.com>
2022-03-02 01:27:37 -0800

    refactor and fix max npc hp

--- misc/max_npc_hp.js
+++ misc/max_npc_hp.js
@@ -1,21 +1,27 @@
 // A simple macro for maximizing NPC HP
 
-const dir = new ActorDirectory();
 const regEx = /(\d+d\d+)/;
 
-dir.documents.forEach(function(obj){
-    let formula, match, math, solution;
-    let attributes = obj.data.data.attr;
-    if(!attributes){ return; }
-    formula = attributes.hp.formula;
-    if(!formula){return;}
-    match    = formula.match(regEx);
-    if(!match){return;}
-    math     = match[0].replace('d','*');
-    math     = '('+math+')';
-    formula  = formula.replace(match[0], math);
-    solution = eval(formula);
-    obj.data.data.attr.hp.value = solution;
-    obj.data.data.attr.hp.max = solution;
-    await obj.update();
+// Choose one of the following to update by uncommenting one of the two lines below
+//const actors = game.actors; // update all actors in the sidebar
+const actors = canvas.tokens.controlled.map((t) => t.actor); // update all selected tokens
+
+actors
+  .filter((actor) => actor.type === "npc")
+  .forEach(async (actor) => {
+    const formula = actor.data.data?.attributes?.hp?.formula;
+    if (!formula) return;
+
+    const match = formula.match(regEx);
+    if (!match) return;
+
+    const maxDice = "(" + match[0].replace("d", "*") + ")";
+    const maxFormula = formula.replace(match[0], maxDice);
+    const maxHp = eval(maxFormula);
+
+    const data = {
+      "data.attributes.hp.value": maxHp,
+      "data.attributes.hp.max": maxHp,
+    };
+    await actor.update(data);
   });
