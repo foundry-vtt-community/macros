8693a053b8f74ec49ab1d0ac7d7cf82b5cee5717
Alexandru Dracea <adracea@gmail.com>
2023-11-30 20:23:03 +0200

    Fix polygon_drawing_to_walls.js for V11
    Changed the macro's logic to account for the changes of API that happened in from V8-9 to V11

--- misc/polygon_drawing_to_walls.js
+++ misc/polygon_drawing_to_walls.js
@@ -3,49 +3,44 @@
  * @Author: cole#9640
  */
 
- function toRadians(degrees)
- {
-   var pi = Math.PI;
-   return degrees * (pi/180);
- }
- 
- 
- let drawings = canvas.drawings.controlled;
- 
- drawings = drawings.filter(drawing =>  {
-     if (!drawing.isPolygon) {
-         ui.notifications.warn(`Drawing "${drawing.data._id}" is not a polygon, skipping`);
-         return false;
-     }
-     return true;
- });
- 
- if (drawings.length) {
-     const newWalls = drawings.flatMap((drawing) => {
-         const { x, y, width, height } = drawing.data;
-         const xCenterOffset = width/2;
-         const yCenterOffset = height/2;
-         
-         const θ = toRadians(drawing.data.rotation);
-         const cosθ = Math.cos(θ);
-         const sinθ = Math.sin(θ);
-         
-         const points = drawing.data.points.map((point) => {
-             const offsetX = point[0] - xCenterOffset;
-             const offsetY = point[1] - yCenterOffset;
-             const rotatedX = (offsetX * cosθ - offsetY * sinθ);
-             const rotatedY = (offsetY * cosθ + offsetX * sinθ);
-             return [rotatedX + x + xCenterOffset, rotatedY + y + yCenterOffset];
-         });
-         
-         return points.slice(0, points.length - 1)
-             .map((point, i) => {
-                 return { c: point.concat(points[i + 1]) };
-             });
-     });
-     
-     canvas.scene.createEmbeddedDocuments("Wall", newWalls);
-     canvas.walls.activate();
- } else {
-     ui.notifications.error("No polygon drawings selected!");
- }
+let drawings = canvas.drawings.controlled;
+
+drawings = drawings.filter(drawing => {
+    if (!drawing.isPolygon) {
+        ui.notifications.warn(`Drawing "${drawing.document._id}" is not a polygon, skipping`);
+        return false;
+    }
+    return true;
+});
+
+if (drawings.length) {
+    const newWalls = drawings.flatMap((drawing) => {
+        const { x, y } = drawing.document;
+        const { width, height } = drawing.document.shape;
+        const xCenterOffset = width / 2;
+        const yCenterOffset = height / 2;
+
+        const o = Math.toRadians(drawing.document.rotation);
+        const coso = Math.cos(o);
+        const sino = Math.sin(o);
+
+        pts = drawing.document.shape.points;
+        points = [];
+        for (i = 0; i < pts.length - 1; i += 2) {
+            const offsetX = pts[i] - xCenterOffset;
+            const offsetY = pts[i + 1] - yCenterOffset;
+            const rotatedX = (offsetX * coso - offsetY * sino);
+            const rotatedY = (offsetY * coso + offsetX * sino);
+            points.push([rotatedX + x + xCenterOffset, rotatedY + y + yCenterOffset]);
+        }
+        return points.slice(0, points.length - 1)
+            .map((point, i) => {
+                return { c: point.concat(points[i + 1]) };
+            });
+    });
+
+    canvas.scene.createEmbeddedDocuments("Wall", newWalls).then(as => { canvas.walls.activate(); });
+
+} else {
+    ui.notifications.error("No polygon drawings selected!");
+}
